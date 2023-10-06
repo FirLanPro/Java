bf42a33850dd93ff4b62b5d9a35b440384f70c37
FirLanPro <FirLanPro@yandex.ru>
2023-10-06 20:56:26 +0300

    Lecture_4

---
+++ Lecture_4/program.java
@@ -0,0 +1,15 @@
+package Lecture_4;
+
+import java.util.LinkedList;
+
+public class program {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<Integer>();
+
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        System.out.println(ll);
+
+    }
+}
---
+++ Lecture_4/program1.java
@@ -0,0 +1,60 @@
+// package Lecture_4;
+
+// import java.util.LinkedList;
+// //import java.util.PriorityQueue;
+// import java.util.Queue;
+
+// public class program1 {
+//     public static void main(String[] args) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        // pq.add(123);
+        // pq.add(3);
+        // pq.add(13);
+        // pq.add(1);
+        // System.out.println(pq);
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+
+        // System.out.println("--------");
+        // Queue<Integer> queue = new LinkedList<Integer>();
+        // queue.add(1);
+        // queue.add(2);
+        // queue.add(3); 
+        // queue.add(4);
+        // System.out.println(queue); // [1, 2, 3, 4]
+        // int item = queue.remove();
+        // System.out.println(queue); // [2, 3, 4]
+        // queue.offer(2809);
+        // item = queue.remove();
+        // System.out.println(queue); // [3, 4, 2809]
+        // item = queue.remove();
+        // System.out.println(queue); // [4, 2809]
+        // item = queue.remove();
+        // System.out.println(queue); // [2809]
+        // System.out.println(queue.peek());
+
+        
+
+
+        // System.out.println("--------");
+        // Queue<Integer> queu = new LinkedList<Integer>();
+        // queu.add(1);
+        // item = queu.remove();
+        // queu.offer(2809);
+        // item = queu.poll();
+        // System.out.println(queu);
+
+        // System.out.println("--------");
+        // Queue<Integer> que = new LinkedList<Integer>();
+        // que.add(1);
+        // item = que.remove();
+        // que.offer(2809);
+        // item = que.poll();
+        // System.out.println(que);
+        // que.element();
+        // que.peek();
+//     }
+// }
