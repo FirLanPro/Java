7678f3836a147bef2f9f788488ba923ea10a8447
FirLanPro <FirLanPro@yandex.ru>
2023-10-12 12:11:53 +0300

    Lecture5

---
+++ Lecture_5/program.java
@@ -0,0 +1,10 @@
+package Lecture_5;
+
+public class program {
+    public static void main(String[] args) {
+        Integer a = 123;
+        System.out.println(a.hashCode());
+        System.out.println("z".hashCode());
+    }
+    
+}
